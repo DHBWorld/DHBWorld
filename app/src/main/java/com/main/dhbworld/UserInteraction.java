c0a29cbdc45216966a2bfc310b2a123989f35219
linus.pust@gmail.com <a3RJ#UfM5I49>
2021-11-15 09:32:01 +0100

    user_interaction_messaging_service_layout.xml wurde um weitere cards erweitert

--- app/src/main/java/com/main/dhbworld/UserInteraction.java
+++ app/src/main/java/com/main/dhbworld/UserInteraction.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserInteraction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_interaction_layout);
     }
-
-
 }
