0e7148a0c70a60a2637768a88ca7f530cb75e889
Linus Marten Pust <linus.pust@siemens.com>
2021-11-16 15:43:57 +0100

    possibly faulty commit. just for transfer to pc.

--- app/src/main/java/com/main/dhbworld/MainActivity.java
+++ app/src/main/java/com/main/dhbworld/MainActivity.java
@@ -2,15 +2,25 @@ package com.main.dhbworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
 
 import com.main.dhbworld.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/com/main/dhbworld/navigation.java
@@ -0,0 +1,4 @@
+package com.main.dhbworld;
+
+public class navigation {
+}
