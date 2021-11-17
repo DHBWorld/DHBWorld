a03bca43386537344a76eb4ea05d52969af636be
Linus Marten Pust <linus.pust@siemens.com>
2021-11-17 13:21:15 +0100

    navigation layout fully added

--- app/src/main/java/com/main/dhbworld/MainActivity.java
+++ app/src/main/java/com/main/dhbworld/MainActivity.java
@@ -1,21 +1,57 @@
 package com.main.dhbworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.main.dhbworld.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+        FloatingActionButton editButton;
+        Button saveButton;
+        Button cancelButton;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+        editButton = findViewById(R.id.edit_Button);
+        saveButton = findViewById(R.id.save_Button);
+        cancelButton = findViewById(R.id.cancel_Button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editButton.setVisibility(View.INVISIBLE);
+                saveButton.setVisibility(View.VISIBLE);
+                cancelButton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
 
 
 
