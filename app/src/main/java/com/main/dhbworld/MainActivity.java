4cab4fc6acb4691ed1ca6c7737cb3d402694acaf
Linus Marten Pust <linus.pust@siemens.com>
2021-11-17 14:30:34 +0100

    buttons now have functions!

--- app/src/main/java/com/main/dhbworld/MainActivity.java
+++ app/src/main/java/com/main/dhbworld/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FloatingActionButton editButton;
         Button saveButton;
         Button cancelButton;
+        TextView name;
+        TextView mNumber;
+        TextView lNumber;
+        TextView studentMail;
+        TextView freeNotes;
+
 
 
 
@@ -31,6 +38,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editButton = findViewById(R.id.edit_Button);
+        saveButton = findViewById(R.id.save_Button);
+        cancelButton = findViewById(R.id.cancel_Button);
+        name = findViewById(R.id.studentName);
+        mNumber = findViewById(R.id.matriculationNumber);
+        lNumber = findViewById(R.id.libraryNumber);
+        studentMail = findViewById(R.id.studentMail);
+        freeNotes = findViewById(R.id.freeNotes);
+
+        name.setEnabled(false);
+        mNumber.setEnabled(false);
+        lNumber.setEnabled(false);
+        studentMail.setEnabled(false);
+        freeNotes.setEnabled(false);
+
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,16 +62,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
-        editButton = findViewById(R.id.edit_Button);
-        saveButton = findViewById(R.id.save_Button);
-        cancelButton = findViewById(R.id.cancel_Button);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editButton.setVisibility(View.INVISIBLE);
-                saveButton.setVisibility(View.VISIBLE);
-                cancelButton.setVisibility(View.VISIBLE);
-            }
+
+
+
+        editButton.setOnClickListener(v -> {
+            editButton.setVisibility(View.INVISIBLE);
+            saveButton.setVisibility(View.VISIBLE);
+            cancelButton.setVisibility(View.VISIBLE);
+
+            name.setEnabled(true);
+            mNumber.setEnabled(true);
+            lNumber.setEnabled(true);
+            studentMail.setEnabled(true);
+            freeNotes.setEnabled(true);
+        });
+
+        saveButton.setOnClickListener(v -> {
+            editButton.setVisibility(View.VISIBLE);
+            saveButton.setVisibility(View.INVISIBLE);
+            cancelButton.setVisibility(View.INVISIBLE);
+
+            name.setEnabled(false);
+            mNumber.setEnabled(false);
+            lNumber.setEnabled(false);
+            studentMail.setEnabled(false);
+            freeNotes.setEnabled(false);
+        });
+
+        cancelButton.setOnClickListener(v -> {
+            editButton.setVisibility(View.VISIBLE);
+            saveButton.setVisibility(View.INVISIBLE);
+            cancelButton.setVisibility(View.INVISIBLE);
+
+            name.setEnabled(false);
+            mNumber.setEnabled(false);
+            lNumber.setEnabled(false);
+            studentMail.setEnabled(false);
+            freeNotes.setEnabled(false);
         });
     }
 
