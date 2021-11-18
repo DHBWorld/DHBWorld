9ff2183d75433d876cd6c3f6c73bf5a6408ef216
linus.pust@gmail.com <a3RJ#UfM5I49>
2021-11-18 19:37:38 +0100

    in user_interaction_layout.xml wurden buttons Ja und Nein eingerichtet

---
+++ app/src/main/java/com/main/dhbworld/Fragments/DialogCofirmationUserInteraction.java
@@ -0,0 +1,44 @@
+package com.main.dhbworld.Fragments;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Gravity;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.main.dhbworld.R;
+
+
+public class DialogCofirmationUserInteraction extends MaterialAlertDialogBuilder {
+
+
+    public DialogCofirmationUserInteraction(@NonNull Context context, int message) {
+        super(context);
+
+        this.setTitle(R.string.sind_sie_sicher);
+        this.setMessage(message);
+
+        this.setNegativeButton(R.string.abbrechen, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+
+        this.setPositiveButton(R.string.event_melden, new DialogInterface.OnClickListener() {
+
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Toast toast= Toast.makeText(context, R.string.danke_fuer_event, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+
+            }
+        });
+    }
+
+  }
--- app/src/main/java/com/main/dhbworld/UserInteraction.java
+++ app/src/main/java/com/main/dhbworld/UserInteraction.java
@@ -1,14 +1,58 @@
 package com.main.dhbworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.main.dhbworld.Fragments.DialogCofirmationUserInteraction;
 
 public class UserInteraction extends AppCompatActivity {
+    private Button[] ja;
+    private Button[] nein;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_interaction_layout);
+        ja =new Button[3];
+        ja[0]= findViewById(R.id.ja0);
+        ja[1]= findViewById(R.id.ja1);
+        ja[2]= findViewById(R.id.ja2);
+
+        nein =new Button[3];
+        nein[0]= findViewById(R.id.nein0);
+        nein[1]= findViewById(R.id.nein1);
+        nein[2]= findViewById(R.id.nein2);
+
+        for(Button j:ja){
+            j.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogCofirmationUserInteraction confirmation= new DialogCofirmationUserInteraction(UserInteraction.this, R.string.moechten_sie_event_melden);
+                    confirmation.show();
+
+                    //Backend logik: Ein Event wurde gemeldet
+
+                }});}
+
+
+
+        for(Button n:nein){
+            n.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogCofirmationUserInteraction confirmation= new DialogCofirmationUserInteraction(UserInteraction.this, R.string.moechten_sie_das_problem_besteht_nicht_mehr);
+                    confirmation.show();
+
+                    //Backend logik: Das Event besteht nicht mehr
+
+                }});}
+
+
     }
 }
