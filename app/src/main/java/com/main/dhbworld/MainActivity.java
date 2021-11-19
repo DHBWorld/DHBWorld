248914b3a502b569f5a73acf89af0190107da729
InFumumVerti <linus.pust@gmail.com>
2021-11-19 14:57:12 +0100

    renamed activity_main.xml to personal_information.xml
    defaultlayout.xml now contains actual default layout

--- app/src/main/java/com/main/dhbworld/MainActivity.java
+++ app/src/main/java/com/main/dhbworld/MainActivity.java
@@ -15,43 +15,13 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-        FloatingActionButton editButton;
-        Button saveButton;
-        Button cancelButton;
-        TextView name;
-        TextView mNumber;
-        TextView lNumber;
-        TextView studentMail;
-        TextView freeNotes;
 
-        SharedPreferences sp;
-        public static final String MyPREFERENCES = "" ;
-        public static final String Name = "";
-        public static final String LibraryNumber = "";
-        public static final String MatriculationNumber = "";
-        public static final String  StudentMail= "";
-        public static final String FreeNotes = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_information);
-
-        editButton = findViewById(R.id.edit_Button);
-        saveButton = findViewById(R.id.save_Button);
-        cancelButton = findViewById(R.id.cancel_Button);
-        name = findViewById(R.id.studentName);
-        mNumber = findViewById(R.id.matriculationNumber);
-        lNumber = findViewById(R.id.libraryNumber);
-        studentMail = findViewById(R.id.studentMail);
-        freeNotes = findViewById(R.id.freeNotes);
-
-        name.setEnabled(false);
-        mNumber.setEnabled(false);
-        lNumber.setEnabled(false);
-        studentMail.setEnabled(false);
-        freeNotes.setEnabled(false);
+        setContentView(R.layout.defaultlayout);
 
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -61,68 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 drawerLayout.openDrawer(GravityCompat.START);
 
             }
-        });
-
-
-        sp = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-
-
-
-
-        editButton.setOnClickListener(v -> {
-            editButton.setVisibility(View.INVISIBLE);
-            saveButton.setVisibility(View.VISIBLE);
-            cancelButton.setVisibility(View.VISIBLE);
-
-            name.setEnabled(true);
-            mNumber.setEnabled(true);
-            lNumber.setEnabled(true);
-            studentMail.setEnabled(true);
-            freeNotes.setEnabled(true);
-
-        });
-
-        saveButton.setOnClickListener(v -> {
-
-            editButton.setVisibility(View.VISIBLE);
-            saveButton.setVisibility(View.INVISIBLE);
-            cancelButton.setVisibility(View.INVISIBLE);
-
-            name.setEnabled(false);
-            mNumber.setEnabled(false);
-            lNumber.setEnabled(false);
-            studentMail.setEnabled(false);
-            freeNotes.setEnabled(false);
-
-            String n = name.getText().toString();
-            String m = mNumber.getText().toString();
-            String l = lNumber.getText().toString();
-            String s = studentMail.getText().toString();
-            String f = freeNotes.getText().toString();
-
-            SharedPreferences.Editor editor = sp.edit();
-
-            editor.putString(Name,n);
-            editor.putString(MatriculationNumber,m);
-            editor.putString(LibraryNumber,l);
-            editor.putString(StudentMail,s);
-            editor.putString(FreeNotes,f);
-            editor.apply();
-
-
-
-        });
-
-        cancelButton.setOnClickListener(v -> {
-            editButton.setVisibility(View.VISIBLE);
-            saveButton.setVisibility(View.INVISIBLE);
-            cancelButton.setVisibility(View.INVISIBLE);
-
-            name.setEnabled(false);
-            mNumber.setEnabled(false);
-            lNumber.setEnabled(false);
-            studentMail.setEnabled(false);
-            freeNotes.setEnabled(false);
         });
 
 
