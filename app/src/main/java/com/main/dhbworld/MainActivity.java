53e6660d324cd4ad3fccd95adb90040f93c9d4cf
InFumumVerti <linus.pust@gmail.com>
2021-11-19 14:37:42 +0100

    renamed activity_main.xml to personal_information.xml

--- app/src/main/java/com/main/dhbworld/MainActivity.java
+++ app/src/main/java/com/main/dhbworld/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,9 +13,6 @@ import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.main.dhbworld.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
         FloatingActionButton editButton;
@@ -40,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.personal_information);
 
         editButton = findViewById(R.id.edit_Button);
         saveButton = findViewById(R.id.save_Button);
