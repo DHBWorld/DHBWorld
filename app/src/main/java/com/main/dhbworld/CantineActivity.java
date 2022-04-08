f0f6c99c371a158f283c16eb2d1b6da13eabbab0
dk1553 <daria-line@mail.ru>
2022-04-08 19:02:05 +0200

    test featureCantine branch

---
+++ app/src/main/java/com/main/dhbworld/CantineActivity.java
@@ -0,0 +1,14 @@
+package com.main.dhbworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CantineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cantine);
+    }
+}
