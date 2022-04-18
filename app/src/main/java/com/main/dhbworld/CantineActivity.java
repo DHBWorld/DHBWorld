6238da5c02288e413c189b543a5f1f89992cd64d
dk1553 <daria-line@mail.ru>
2022-04-18 21:06:11 +0200

    UC "show weekly mealplan": Aufräumung vom Code

--- app/src/main/java/com/main/dhbworld/CantineActivity.java
+++ app/src/main/java/com/main/dhbworld/CantineActivity.java
@@ -48,8 +48,8 @@ public class CantineActivity extends AppCompatActivity {
     private LinearLayout layoutMealCardsBasic;
     private LinearLayout layoutMealCardsExtra;
     private String inputFromApi;
-
-
+    TabLayout tabLayout;
+    Date[] currentWeek;
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -59,26 +59,62 @@ public class CantineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cantine);
         layoutMealCardsBasic= findViewById(R.id.layoutMealCardsBasic);
         layoutMealCardsExtra= findViewById(R.id.layoutMealCardsExtra);
+        tabLayout= findViewById(R.id.tabTags);
 
+        generateCurrentWeek();
+        showTabs();
         loadDisplay("Daten werden gleich geladen");
 
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+
+
+                SimpleDateFormat f =new SimpleDateFormat("dd");
+                try {
+
+                    switch (tab.getPosition()){
+                        case 0: mealPlanFromOpenMensa(currentWeek[2]);
+                            break;
+                        case 1:mealPlanFromOpenMensa(currentWeek[3]);
+                            break;
+                        case 2: mealPlanFromOpenMensa(currentWeek[4]);
+                            break;
+                        case 3: mealPlanFromOpenMensa(currentWeek[0]);
+                            break;
+                        case 4: mealPlanFromOpenMensa(currentWeek[1]);
+                                break;
+                    }
+                } catch ( IOException e) {
+                            loadDisplay("Es ist ein Fehler aufgetaucht...");
+                            e.printStackTrace();
+                        }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+
+
+
+
+    }
+
+    private void generateCurrentWeek(){
         Date date= new Date();
-
-        Date[] currentWeek = new Date[5];
-        //inputFromApi ="[{\"id\":8495433,\"name\":\"Börek mit Rindfleischfüllung, Gemüse-Couscous, Bolognesesoße\",\"category\":\"Wahlessen 1\",\"prices\":{\"students\":2.6,\"employees\":3.5,\"pupils\":2.95,\"others\":4.0},\"notes\":[\"mit Farbstoff \",\"Eier\",\"Haselnüsse\",\"Milch/Laktose\",\"Sellerie\",\"Senf\",\"Soja\",\"Weizen\",\"enthält Rindfleisch\"]},{\"id\":8495434,\"name\":\"Börek mit Spinatfüllung, Gemüse-Couscous, pikanter Soja Dip\",\"category\":\"Wahlessen 2\",\"prices\":{\"students\":2.6,\"employees\":3.5,\"pupils\":2.95,\"others\":4.0},\"notes\":[\"mit Farbstoff \",\"mit Konservierungsstoff \",\"mit Antioxidationsmittel\",\"mit Geschmacksverstärker \",\"Sellerie\",\"Soja\",\"Weizen\",\"veganes Gericht\"]},{\"id\":8441249,\"name\":\"Verschiedene Dessert\",\"category\":\"Wahlessen 3\",\"prices\":{\"students\":0.95,\"employees\":0.95,\"pupils\":0.95,\"others\":1.15},\"notes\":[\"mit Farbstoff \",\"Milch/Laktose\",\"vegetarisches Gericht\"]},{\"id\":8441251,\"name\":\"Grüner Mischsalat\",\"category\":\"Wahlessen 3\",\"prices\":{\"students\":0.8,\"employees\":0.8,\"pupils\":0.8,\"others\":1.0},\"notes\":[\"mit Antioxidationsmittel\",\"Sellerie\",\"veganes Gericht\"]},{\"id\":8441250,\"name\":\"Blumenkohlgemüse\",\"category\":\"Wahlessen 3\",\"prices\":{\"students\":0.8,\"employees\":0.8,\"pupils\":0.8,\"others\":1.0},\"notes\":[\"mit Farbstoff \",\"Sellerie\",\"veganes Gericht\"]},{\"id\":8444721,\"name\":\"Gemüse-Couscous\",\"category\":\"Wahlessen 3\",\"prices\":{\"students\":0.75,\"employees\":0.75,\"pupils\":0.75,\"others\":0.95},\"notes\":[\"mit Farbstoff \",\"Sellerie\",\"Weizen\",\"veganes Gericht\"]}]";
-
-
-
-        TabLayout tabLayout= findViewById(R.id.tabTags);
-
+        currentWeek = new Date[5];
         Calendar c= Calendar.getInstance();
         c.setTime(date);
-        Integer dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-        System.out.println(")))))))))))))))))))))))))))");
-        System.out.println(dayOfWeek); // 2 ->Mo
-
+        Integer dayOfWeek = c.get(Calendar.DAY_OF_WEEK); // dayOfWeek2 ->Mo
         currentWeek[dayOfWeek]=date;
+
 
         for (int q=dayOfWeek+1; q<5;q++){
             c.add(Calendar.DATE, 1);
@@ -88,104 +124,16 @@ public class CantineActivity extends AppCompatActivity {
             c.add(Calendar.DATE, 1);
             currentWeek[q]=c.getTime();
         }
+    }
 
-        System.out.println("§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+    private void showTabs (){
 
-
-        for (int q=0; q<5;q++){
-            SimpleDateFormat format =new SimpleDateFormat("dd.MM.yyyy");
-
-            System.out.println(format.format(currentWeek[q]));
-        }
-
-
-
-        switch (dayOfWeek){
-            case 2:
-        }
         SimpleDateFormat f =new SimpleDateFormat("dd");
         tabLayout.getTabAt(0).setText("Mo ("+f.format(currentWeek[2])+")");
         tabLayout.getTabAt(1).setText("Di ("+f.format(currentWeek[3])+")");
         tabLayout.getTabAt(2).setText("Mi ("+f.format(currentWeek[4])+")");
         tabLayout.getTabAt(3).setText("Do ("+f.format(currentWeek[0])+")");
         tabLayout.getTabAt(4).setText("Fr ("+f.format(currentWeek[1])+")");
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
-
-
-                SimpleDateFormat f =new SimpleDateFormat("dd");
-                switch (tab.getPosition()){
-                    case 0:
-                        try {
-                            mealPlanFromOpenMensa(currentWeek[2]);
-
-                        } catch ( IOException e) {
-                            loadDisplay("Es ist ein Fehler aufgetaucht...");
-                            e.printStackTrace();
-                        }
-
-                        break;
-                    case 1:
-                        try {
-                            mealPlanFromOpenMensa(currentWeek[3]);
-
-                        } catch ( IOException e) {
-                            loadDisplay("Es ist ein Fehler aufgetaucht...");
-                            e.printStackTrace();
-                        }
-
-                        break;
-                    case 2:
-                        try {
-                            mealPlanFromOpenMensa(currentWeek[4]);
-
-                        } catch ( IOException e) {
-                            loadDisplay("Es ist ein Fehler aufgetaucht...");
-                            e.printStackTrace();
-                        }
-
-                        break;
-                    case 3:
-                        try {
-                            mealPlanFromOpenMensa(currentWeek[0]);
-
-                        } catch ( IOException e) {
-                            loadDisplay("Es ist ein Fehler aufgetaucht...");
-                            e.printStackTrace();
-                        }
-
-                        break;
-                    case 4:
-                        try {
-                            mealPlanFromOpenMensa(currentWeek[1]);
-
-                        } catch ( IOException e) {
-                            loadDisplay("Es ist ein Fehler aufgetaucht...");
-                            e.printStackTrace();
-                        }
-
-                        break;
-                }
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-
-
 
     }
 
@@ -239,11 +187,11 @@ public class CantineActivity extends AppCompatActivity {
             LinearLayout cardLayout = new LinearLayout(CantineActivity.this);
             cardLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             cardLayout.setOrientation(LinearLayout.HORIZONTAL);
-            cardLayout.setPadding(0,20,25,20);
+            cardLayout.setPadding(0,20,0,20);
             mealCard.addView(cardLayout);
 
             LinearLayout mealLayout = new LinearLayout(CantineActivity.this);
-            mealLayout.setLayoutParams(new ViewGroup.LayoutParams(780, ViewGroup.LayoutParams.WRAP_CONTENT));
+            mealLayout.setLayoutParams(new ViewGroup.LayoutParams(810, ViewGroup.LayoutParams.WRAP_CONTENT));
             mealLayout.setOrientation(LinearLayout.VERTICAL);
             mealLayout.setPadding(0,0,20,0);
             cardLayout.addView(mealLayout);
@@ -259,6 +207,7 @@ public class CantineActivity extends AppCompatActivity {
             HorizontalScrollView chipScroll = new HorizontalScrollView(CantineActivity.this);
             chipScroll.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             chipScroll.setHorizontalScrollBarEnabled(false);
+            chipScroll.setOverScrollMode(View.OVER_SCROLL_NEVER);
             mealLayout.addView(chipScroll);
 
             LinearLayout chipLayout = new LinearLayout(CantineActivity.this);
@@ -280,14 +229,14 @@ public class CantineActivity extends AppCompatActivity {
             LinearLayout preisLayout = new LinearLayout(CantineActivity.this);
             preisLayout.setLayoutParams(new ViewGroup.LayoutParams(150, ViewGroup.LayoutParams.MATCH_PARENT));
             preisLayout.setOrientation(LinearLayout.VERTICAL);
-            preisLayout.setBackgroundColor(getResources().getColor(R.color.grey_dark));
+            preisLayout.setBackgroundColor(getResources().getColor(R.color.grey_light));
             preisLayout.setGravity(Gravity.CENTER);
 
             cardLayout.addView(preisLayout);
 
             TextView preisView = new TextView(CantineActivity.this);
             preisView.setTextSize(18);
-            preisView.setTextColor(getResources().getColor(R.color.white));
+            preisView.setTextColor(getResources().getColor(R.color.grey_dark));
             preisView.setText(mealDailyPlan.getMeal()[i].getPrice());
             preisView.setGravity(Gravity.CENTER);
             preisView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -375,7 +324,6 @@ public class CantineActivity extends AppCompatActivity {
         errorView.setTextColor(getResources().getColor(R.color.black));
         errorView.setText(message);
         errorView.setPadding(0,0,0,15);
-        //mealView.setGravity(Gravity.CENTER);
         errorView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         layoutMealCardsBasic.addView(errorView);
     }
