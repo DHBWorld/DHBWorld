db2334f3fb8be018fed39cf484811976c3b5ff2f
InFumumVerti <linus.pust@gmail.com>
2022-05-06 12:07:03 +0200

    partial commit

---
+++ app/src/main/java/com/main/dhbworld/Course.java
@@ -0,0 +1,36 @@
+package com.main.dhbworld;
+
+public class Course {
+    String name;
+    int year;
+    String study;
+    String roomNo;
+
+
+//    public Course(String name, int year, String study, String roomNo) {
+//        this.name = name;
+//        this.year = year;
+//        this.study = study;
+//        this.roomNo = roomNo;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getStudy() {
+        return study;
+    }
+
+    public String getRoomNo() {
+        return roomNo;
+    }
+
+
+
+
+}
--- app/src/main/java/com/main/dhbworld/OrganizerActivity.java
+++ app/src/main/java/com/main/dhbworld/OrganizerActivity.java
@@ -1,32 +1,21 @@
 package com.main.dhbworld;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Xml;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.main.dhbworld.Navigation.NavigationUtilities;
 
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrganizerActivity extends AppCompatActivity {
-    ArrayList<String> xmlList = new ArrayList<>();
-    String rawXML;
+    private ArrayList<Course> courses = new ArrayList<>();
 
 
 
@@ -42,60 +31,56 @@ public class OrganizerActivity extends AppCompatActivity {
     }
 
     public List parse() throws Exception {
-        String inputString = getStringFromFile("D:\\Code\\DHBWorld\\rapla.xml");
-        InputStream in = new FileInputStream(inputString);
+        InputStream in = new FileInputStream("D:\\Code\\DHBWorld\\rapla.xml");
 
         try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(false);
+            XmlPullParser parser = factory.newPullParser();
+
             parser.setInput(in, null);
-            parser.nextTag();
-            return readFeed(parser);
+
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tag = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if (tag.equalsIgnoreCase("kurs")) {
+                            Course course = new Course();
+                        }
+                        break;
+
+
+                }
+            }
         } finally {
             in.close();
         }
-    }
-
-    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
-
-        parser.require(XmlPullParser.START_TAG, ns, "feed");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            // Starts by looking for the entry tag
-            if (name.equals("entry")) {
-                entries.add(readEntry(parser));
-            } else {
-                skip(parser);
-            }
-        }
-        return entries;
+        return x;
     }
 
 
 
 
-    public static String convertStreamToString(InputStream is) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        reader.close();
-        return sb.toString();
-    }
-
-
-    public static String getStringFromFile (String filePath) throws Exception {
-        File fl = new File(filePath);
-        FileInputStream fin = new FileInputStream(fl);
-        String ret = convertStreamToString(fin);
-        //Make sure you close all streams.
-        fin.close();
-        return ret;
-    }
+//
+//    public static String convertStreamToString(InputStream is) throws Exception {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        StringBuilder sb = new StringBuilder();
+//        String line = null;
+//        while ((line = reader.readLine()) != null) {
+//            sb.append(line).append("\n");
+//        }
+//        reader.close();
+//        return sb.toString();
+//    }
+//
+//
+//    public static String getStringFromFile (String filePath) throws Exception {
+//        File fl = new File(filePath);
+//        FileInputStream fin = new FileInputStream(fl);
+//        String ret = convertStreamToString(fin);
+//        //Make sure you close all streams.
+//        fin.close();
+//        return ret;
+//    }
 }
