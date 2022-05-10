e510744805c236277f257ad88d64e977dc4a23ba
blitzdose <blitzdose@gmail.com>
2022-05-10 15:15:59 +0200

    Implemented Licenses and changed data privacy

--- app/src/main/java/com/main/dhbworld/DataPrivacyActivity.java
+++ app/src/main/java/com/main/dhbworld/DataPrivacyActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.internal.StringResourceValueReader;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class DataPrivacyActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class DataPrivacyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataprivacy);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         TextView textView = findViewById(R.id.dataprivacytext);
         String text = getResources().getString(R.string.data_privacy);
         textView.setText(Html.fromHtml(text,1));
--- app/src/main/java/com/main/dhbworld/LicenseActivity.java
+++ app/src/main/java/com/main/dhbworld/LicenseActivity.java
@@ -1,8 +1,16 @@
 package com.main.dhbworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class LicenseActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class LicenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licence);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        String[] licenses = getResources().getStringArray(R.array.licenses);
+        String[] licenseTitles = getResources().getStringArray(R.array.license_titles);
+
+        ListView licenseListView = findViewById(R.id.license_listview);
+        licenseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                new MaterialAlertDialogBuilder(LicenseActivity.this)
+                        .setTitle(licenseTitles[position])
+                        .setMessage(licenses[position])
+                        .setPositiveButton(getResources().getString(R.string.close), null)
+                        .show();
+            }
+        });
     }
 }
