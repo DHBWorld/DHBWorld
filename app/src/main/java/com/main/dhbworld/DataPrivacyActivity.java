d63a4d01980ca8bbd74988f7c9c65db2e91254ba
blitzdose <blitzdose@gmail.com>
2022-05-10 15:16:57 +0200

    Cleaned up code

--- app/src/main/java/com/main/dhbworld/DataPrivacyActivity.java
+++ app/src/main/java/com/main/dhbworld/DataPrivacyActivity.java
@@ -1,13 +1,11 @@
 package com.main.dhbworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.common.internal.StringResourceValueReader;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class DataPrivacyActivity extends AppCompatActivity {
@@ -18,12 +16,7 @@ public class DataPrivacyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dataprivacy);
 
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        toolbar.setNavigationOnClickListener(v -> finish());
 
         TextView textView = findViewById(R.id.dataprivacytext);
         String text = getResources().getString(R.string.data_privacy);
