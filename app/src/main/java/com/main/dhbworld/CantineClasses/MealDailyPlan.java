e8f5b417b47ef66a50dcb7af0fb4d9f500cb87f1
dk1553 <daria-line@mail.ru>
2022-05-19 10:52:38 +0200

    Code clean up

--- app/src/main/java/com/main/dhbworld/CantineClasses/MealDailyPlan.java
+++ app/src/main/java/com/main/dhbworld/CantineClasses/MealDailyPlan.java
@@ -1,7 +1,5 @@
 package com.main.dhbworld.CantineClasses;
 
-import com.main.dhbworld.CantineClasses.Meal;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,35 +10,23 @@ public class MealDailyPlan {
     public MealDailyPlan(String inputFromApi) throws JSONException {
         //create JSONArray from input of OpennMensa
         JSONArray jsonArrayMeal = new JSONArray(inputFromApi);
-
         //Translate json-objects into meals
         meals= new Meal[jsonArrayMeal.length()];
         for (int i=0; i<jsonArrayMeal.length();i++){
-
             JSONObject jsonMeal = jsonArrayMeal.getJSONObject(i);
             meals[i]= new Meal();
-
             meals[i].setName(jsonMeal.getString("name"));
             meals[i].setCategory(jsonMeal.getString("category"));
-
             JSONObject jsonPrices = new JSONObject(jsonMeal.getString("prices"));
             meals[i].setPrice(jsonPrices.getString("students"));
-
             JSONArray jsonNotes = new JSONArray(jsonMeal.getString("notes"));
             for (int j=0; j<jsonNotes.length();j++){
                 meals[i].addNote(jsonNotes.getString(j));
             }
-
-
         }
-
     }
 
-    public Meal[] getMeal() {
-        return meals;
-    }
+    public Meal[] getMeal() { return meals;  }
 
-    public void setMeal(Meal[] meal) {
-        this.meals = meal;
-    }
+    public void setMeal(Meal[] meal) {  this.meals = meal; }
 }
