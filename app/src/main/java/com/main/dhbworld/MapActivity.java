6c78f0a40aea6fb527293813c5c6b4b9641cd92b
blitzdose <blitzdose@gmail.com>
2022-10-05 17:20:37 +0200

    - code cleanup - added translations

--- app/src/main/java/com/main/dhbworld/MapActivity.java
+++ app/src/main/java/com/main/dhbworld/MapActivity.java
@@ -1,15 +1,13 @@
 package com.main.dhbworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jsibbold.zoomage.ZoomageView;
 import com.main.dhbworld.Navigation.NavigationUtilities;
@@ -18,19 +16,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MapActivity extends AppCompatActivity {
+
+    AutoCompleteTextView roomDropdown;
+    AutoCompleteTextView entryDropdown;
+    Button showMapButton;
+    ZoomageView mapImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +40,52 @@ public class MapActivity extends AppCompatActivity {
 
         NavigationUtilities.setUpNavigation(this, R.id.map);
 
-        AutoCompleteTextView roomDropdown = findViewById(R.id.mapRoomDropdown);
-        AutoCompleteTextView entryDropdown = findViewById(R.id.mapEntryDropdown);
+        initializeViews();
 
-        Button button = findViewById(R.id.showMapButton);
+        setRooms();
 
+        ArrayList<String> entryItems = setEntries();
+
+        showMapButton.setOnClickListener(view -> {
+            hideKeyboard();
+
+            String selectedRoom = roomDropdown.getText().toString();
+            String selectedEntry = entryDropdown.getText().toString();
+
+            Drawable map = getImage(selectedRoom, selectedEntry, entryItems);
+            mapImageView.setImageDrawable(map);
+        });
+    }
+
+    private void hideKeyboard() {
+        try {
+            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception ignored) { }
+
+        roomDropdown.clearFocus();
+        entryDropdown.clearFocus();
+    }
+
+    private ArrayList<String> setEntries() {
+        ArrayList<String> entryItems = new ArrayList<>();
+        entryItems.add(getString(R.string.main_entry));
+        entryItems.add(getString(R.string.left));
+        entryItems.add(getString(R.string.right));
+        ArrayAdapter<String> arrayAdapterEntry = new ArrayAdapter<>(this, R.layout.dualis_semester_list_item, entryItems);
+        entryDropdown.setAdapter(arrayAdapterEntry);
+        entryDropdown.setText(entryItems.get(0), false);
+        return entryItems;
+    }
+
+    private void setRooms() {
+        ArrayList<String> roomItems = getRoomItems();
+
+        ArrayAdapter<String> arrayAdapterRoom = new ArrayAdapter<>(this, R.layout.dualis_semester_list_item, roomItems);
+        roomDropdown.setAdapter(arrayAdapterRoom);
+    }
+
+    private ArrayList<String> getRoomItems() {
         ArrayList<String> roomItems = new ArrayList<>();
 
         Map<String, String> fileMap = loadFileMap();
@@ -51,38 +93,14 @@ public class MapActivity extends AppCompatActivity {
             roomItems.add(file.replace(".png", ""));
         }
         Collections.sort(roomItems);
-        ArrayAdapter<String> arrayAdapterRoom = new ArrayAdapter<>(this, R.layout.dualis_semester_list_item, roomItems);
-        roomDropdown.setAdapter(arrayAdapterRoom);
+        return roomItems;
+    }
 
-        ArrayList<String> entryItems = new ArrayList<>();
-        entryItems.add("Haupteingang");
-        entryItems.add("Links");
-        entryItems.add("Rechts");
-        ArrayAdapter<String> arrayAdapterEntry = new ArrayAdapter<>(this, R.layout.dualis_semester_list_item, entryItems);
-        entryDropdown.setAdapter(arrayAdapterEntry);
-        entryDropdown.setText(entryItems.get(0), false);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                } catch (Exception ignored) { }
-
-                String selectedRoom = roomDropdown.getText().toString();
-                String selectedEntry = entryDropdown.getText().toString();
-
-                roomDropdown.clearFocus();
-                entryDropdown.clearFocus();
-
-                //ImageView mapImageView = findViewById(R.id.mapImageView);
-                ZoomageView mapImageView = findViewById(R.id.mapImageView);
-
-                Drawable map = getImage(selectedRoom, selectedEntry, entryItems);
-                mapImageView.setImageDrawable(map);
-            }
-        });
+    private void initializeViews() {
+        roomDropdown = findViewById(R.id.mapRoomDropdown);
+        entryDropdown = findViewById(R.id.mapEntryDropdown);
+        showMapButton = findViewById(R.id.showMapButton);
+        mapImageView = findViewById(R.id.mapImageView);
     }
 
     private Drawable getImage(String selectedRoom, String selectedEntry, ArrayList<String> entryItems) {
@@ -132,9 +150,7 @@ public class MapActivity extends AppCompatActivity {
                 }
 
             });
-        } catch (IOException | JSONException ignored) {
-
-        }
+        } catch (IOException | JSONException ignored) { }
 
         return fileMap;
     }
