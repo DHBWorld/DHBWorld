187aeb588fda84f8ebeeaed9187dc0a21b92efe4
infumumverti <linus.pust@gmail.com>
2022-12-09 23:55:09 +0100

    first success, let's fucking go!

--- app/src/main/java/com/main/dhbworld/Calendar/EventWStyle.java
+++ app/src/main/java/com/main/dhbworld/Calendar/EventWStyle.java
@@ -12,14 +12,16 @@ public class EventWStyle {
     Calendar startTime;
     Calendar endTime;
     String description;
+    String styleString;
     WeekViewEntity.Style style;
 
-    EventWStyle(long id, String title, Calendar startTime, Calendar endTime, String description) {
+    EventWStyle(long id, String title, Calendar startTime, Calendar endTime, String description, String styleString) {
         this.id = id;
         this.title = title;
         this.startTime = startTime;
         this.endTime = endTime;
         this.description = description;
+        this.style = new WeekViewEntity.Style.Builder().setBackgroundColor(styleString.hashCode()).build();
     }
 
     public long getId() {
@@ -45,8 +47,5 @@ public class EventWStyle {
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public void setStyle(String color){
-    style = new WeekViewEntity.Style.Builder().setBackgroundColor(color.hashCode()).build(); }
 
 }
